40cc6136a5d99e1ee84abbbc7da9236d007e2458
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-01-23 02:47:48 +0800

    Refactor: make bot code a module

---
+++ bot/commands/__init__.py
@@ -0,0 +1,3 @@
+from bot.commands.CommandCollector import getAllCommands
+
+__all__ = ['getAllCommands']
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 
-from commands.CommandCollector import getAllCommands
+import bot.commands
 
 if __name__ == "__main__":
     load_dotenv()
     
     app = ApplicationBuilder().token(os.getenv("BOT_API_TOKEN")).build()
     
-    for command in getAllCommands():
+    for command in bot.commands.getAllCommands():
         command_handler = CommandHandler(command.__name__, command)
         app.add_handler(command_handler)
     
