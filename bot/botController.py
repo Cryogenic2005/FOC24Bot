9331a66df1a62de75214cbbd6562c12dd13be3e5
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-01-24 20:49:56 +0800

    feat: add constants for controlling bot's activity

--- bot/botController.py
+++ bot/botController.py
@@ -4,14 +4,17 @@ class BotStatus:
     Note: No instances need to be created
     '''
     
-    _BOT_ACTIVE = False
+    _isBotActive = False
+    
+    ACTIVE = True
+    INACTIVE = False
 
     @classmethod
     def isBotActive(cls):
         '''Check if bot is active'''
-        return BotStatus._BOT_ACTIVE
+        return BotStatus._isBotActive
 
     @classmethod
     def setBotStatus(cls, status: bool):
         '''Set the status of the bot to be active (True) or inactive (False)'''
-        BotStatus._BOT_ACTIVE = status
+        BotStatus._isBotActive = status
