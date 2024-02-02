6f70f29fea352bae3d7d7223106221f7dfeb0c04
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-02-02 20:42:34 +0800

    chore: Implement added commands to bot

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,5 +1,7 @@
+from .gameCommands import group
 from .miscCommands import hello, help, enable, disable, start
+from .permsManagementCommands import grant, demote, check_perms
 
-commandList = [hello, help, enable, disable, start]
+commandList = [hello, help, enable, disable, start, group, grant, demote, check_perms]
 
 __all__ = ['commandList']
