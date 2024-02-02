1cc505cc477e9796013cc8585617a25369f8e343
Cryogenic2005 <97326954+Cryogenic2005@users.noreply.github.com>
2024-02-02 21:46:13 +0800

    fix: add filters to filter out edited messages

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,10 +1,14 @@
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, filters
 
 from .commands import commandList
 
 handlers = []
 
 for command in commandList:
-    handlers.append(CommandHandler(command=command.__name__, callback=command))
+    handlers.append(CommandHandler(
+        command=command.__name__,
+        callback=command,
+        filters=filters.COMMAND & ~filters.UpdateType.EDITED_MESSAGE
+    ))
     
 __all__ = ['handlers']
